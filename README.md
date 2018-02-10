cf3b217994667a0bf67df779d5eef847e24b568b
Adriano Gil <adrianogil.san@gmail.com>
2018-02-10 12:20:24 -0400

    Added save and list commands

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Okane
 お金: Personal finance manager via command line
+
+
+# Command line options
+
+Save a register
+
+```
+okane -s <description> <amount> <date>
+```
+
+List financial registers
+```
+okane -l
+```
+
+
+Show a balance
+```
+okane -l
+```
---
+++ src/okane.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+import sys, sqlite3, os, datetime
+from dateutil.parser import parse as dtparse
+import utils
+
+list_args = '--save -s --list -l'
+
+# Open Connection
+okane_directory = os.environ['OKANE_DIR'] + '/db/'
+conn = sqlite3.connect(okane_directory + 'okane.sqlite');
+
+# Creating cursor
+c = conn.cursor()
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+def create_tables():
+    # Create table
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS Categories (
+            id_category INTEGER,
+            category_name TEXT,
+            PRIMARY KEY (id_category)
+        )
+    ''')
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS FinancialRegisters (
+            id_register INTEGER,
+            description TEXT,
+            amount REAL,
+            register_dt TEXT,
+            PRIMARY KEY (id_register)
+        )
+    ''')
+
+def save_register(args):
+    if len(args) == 2:
+        # Save current register
+        sql_query_save = "INSERT INTO FinancialRegisters (description, amount, register_dt)" + \
+                        " VALUES (:description,:amount,:register_dt)"
+        
+        now = datetime.datetime.now()
+        save_data = (args[0], float(args[1]), now.strftime("%Y-%m-%d %H:%M:%S")) # YYYY-MM-DD HH:MM:SS.SSS
+
+        c.execute(sql_query_save, save_data)
+        conn.commit()
+        print '.'
+
+def show_registers(args):
+    if len(args) == 0:
+        c.execute("SELECT * from FinancialRegisters ORDER BY date(register_dt)")
+        for row in c:
+            reg_date = str(row[3])
+            row_data = (str(row[0]), reg_date, str(row[1]), str(row[2]))
+            row_text = bcolors.OKBLUE + 'Id:' + bcolors.ENDC + ' %s\t' + \
+                       bcolors.OKBLUE + 'Date:' + bcolors.ENDC + ' %s\t' + \
+                       bcolors.OKBLUE + 'Amount:' + bcolors.ENDC + ' %s\t' + \
+                       bcolors.OKBLUE + 'Description:' + bcolors.ENDC + ' %s'
+            print(row_text % row_data )
+
+commands_parse = {
+    '-s' : save_register,
+    '-l' : show_registers
+}
+
+def parse_arguments():
+
+    args = {}
+
+    last_key = ''
+
+    for i in xrange(1, len(sys.argv)):
+        a = sys.argv[i]
+        if a[0] == '-' and not utils.is_float(a):
+            last_key = a
+            args[a] = []
+        elif last_key != '':
+            arg_values = args[last_key]
+            arg_values.append(a)
+            args[last_key] = arg_values
+
+    return args
+
+def parse_commands(args):
+    for a in args:
+        if a in commands_parse:
+            commands_parse[a](args[a])
+
+create_tables()
+args = parse_arguments()
+parse_commands(args)

0e14990c095be68ff6722523ed5f7f987a35a46c
Adriano Gil <adrianogil.san@gmail.com>
2023-11-05 11:28:14 -0400

    refactor: using pyutils to generate properties definition for MoneyRegister
    - added method get_data to return properties as a dictionary

--- src/python/okane/entity/moneyregister.py
+++ src/python/okane/entity/moneyregister.py
@@ -1,28 +1,36 @@
-import datetime
+from pyutils.code.props.classproperties import declare_props
+
+from datetime import datetime
 
 class MoneyRegister:
     def __init__(self, args):
-        self.id = -1
-        if 'amount' in args:
-            self.amount = args['amount']
-        else:
-            self.amount = 0
-
-        if 'description' in args:
-            self.description = args['description']
-        else:
-            self.description = ""
-
-        if 'register_dt' in args:
-            self.register_dt = args['register_dt']
-        else:
-            self.register_dt = datetime.datetime.now()
-
-        if 'category' in args:
-            self.category = args['category']
-
-        if 'account' in args:
-            self.account = args['account']
+        declare_props(
+            self,
+            props={
+                'id': {
+                    'prop_type': int,
+                    'default_value': -1
+                },
+                'name': {
+                    'prop_type': str,
+                    'default_value': ''
+                },
+                'amount': {
+                    'prop_type': float,
+                    'default_value': 0.0
+                },
+                'description': {
+                    'prop_type': str,
+                    'default_value': ''
+                },
+                'register_dt': {
+                    'default_value': datetime.now()
+                },
+                'category': {},
+                'account': {}
+            }
+        )
+        self.set_properties(args)
 
     def __str__(self):
         return "(ID: " + str(self.id) + \
@@ -33,7 +41,7 @@ class MoneyRegister:
 
 
     def get_register_dt(self):
-        return self.register_dt.strftime("%Y-%m-%d %H:%M:%S")
+        return self.register_dt.strftime("%Y-%m-%d")
 
     def get_data_tuple(self):
         return  (self.description, \
@@ -41,6 +49,11 @@ class MoneyRegister:
                      self.get_register_dt(),\
                      self.category.id,\
                      self.account.id)
+    
+    def get_data(self):
+        properties_data = self.get_properties()
+        properties_data['category'] = properties_data['category'].name
+        properties_data['account'] = properties_data['account'].name
+        properties_data['register_dt'] = self.get_register_dt()
 
-
-
+        return properties_data
